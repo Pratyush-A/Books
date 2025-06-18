104c81d2e5464bc336d5baffaecdbe3851766a72
Pratyush-A <pratyushacharya34@email.com>
2025-06-18 06:54:20 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import authRoutes from './Routes/authRoutes.js';
 import bookRoutes from './Routes/bookRoutes.js';
-
+import job from './lib/cron.js';
 
 import {connectDB} from './lib/db.js';
 // Connect to the database
@@ -11,6 +11,8 @@ import {connectDB} from './lib/db.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+job.start();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
